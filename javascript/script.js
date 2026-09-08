20e234784ecb4adf9cb17aff4df2702f957afe34
Nicko2009 <niwea005@osloskolen.no>
2026-09-08 09:19:07 +0200

    spørmsål liste

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function start() {
+document.getElementById("sporsmol").innerHTML = "spørsmål 1"
+document.getElementById("sporsmol1").innerHTML = "Hva er en IP-addresse"
+document.getElementById("svar1").innerHTML = "Et passord"
+document.getElementById("svar2").innerHTML = "Et type harddisk"
+document.getElementById("svar3").innerHTML = "Et programmeringsspråk"
+document.getElementById("svar4").innerHTML = "En type harddisk"
+}
